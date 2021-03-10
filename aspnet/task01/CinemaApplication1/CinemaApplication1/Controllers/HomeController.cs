bc496c3646815a77ebc9cee5188e8d8224c007de
Garibov2001 <mahmood.garibov@gmail.com>
2021-03-10 21:38:49 +0400

    Details page is done

--- aspnet/task01/CinemaApplication1/CinemaApplication1/Controllers/HomeController.cs
+++ aspnet/task01/CinemaApplication1/CinemaApplication1/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace CinemaApplication1.Controllers
 
             return View();
         }
+
+        public ActionResult Detail()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
